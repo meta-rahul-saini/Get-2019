12afac7a7d27396efbc96652d08462600368bca7
meta-rahul-saini <52689695+meta-rahul-saini@users.noreply.github.com>
2019-07-19 07:27:11 +0530

    Update ArrOperations.java

--- ASSIGNMENT4/src/ArrOperations.java
+++ ASSIGNMENT4/src/ArrOperations.java
@@ -2,26 +2,35 @@ public class ArrOperations {
 
 	/**
 	 * this function finds the maxMirror in the array
-	 * @param nums
-	 *            array of integers
+	 * @param arrayOfIntegers, array of integers
 	 * @return length of maxMirror, and throw assertion error if array is empty.
 	 */
-	public int findMaxMirror(int[] nums) {
+	public int findMaxMirror(int[] arrayOfIntegers) {
 		int maxLengthMirror = 0;
 		int mirrorLength = 0;
-		
-		if(nums.length==0)
+		final int LENGTH_OF_ARRAY = arrayOfIntegers.length;
+
+		if (LENGTH_OF_ARRAY == 0) 
 		{
 			throw new AssertionError("Assertion Error Occured: Please don't pass an empty array");
 		}
-		for (int i = 0; i < nums.length; i++) {
-			for (int j = nums.length - 1; j >= 0
-					&& i + mirrorLength < nums.length; j--) {
-				if (nums[i + mirrorLength] == nums[j]) {
+		
+		// finding length of max mirror
+		
+		for (int indexFromLeft = 0; indexFromLeft < LENGTH_OF_ARRAY; indexFromLeft++) 
+		{
+			for (int indexFromRight = LENGTH_OF_ARRAY - 1; indexFromRight >= 0
+					&& indexFromLeft + mirrorLength < LENGTH_OF_ARRAY; indexFromRight--) 
+			{
+				if (arrayOfIntegers[indexFromLeft + mirrorLength] == arrayOfIntegers[indexFromRight]) 
+				{
 					mirrorLength++;
-				} else {
-					if (maxLengthMirror < mirrorLength) {
-						maxLengthMirror = mirrorLength;   
+				} 
+				else 
+				{
+					if (maxLengthMirror < mirrorLength) 
+					{
+						maxLengthMirror = mirrorLength;
 					}
 					mirrorLength = 0;
 				}
@@ -34,133 +43,155 @@ public class ArrOperations {
 		return maxLengthMirror;
 	}
 
-	
-	/** returns the number of clumps in input array, throws assertion error if array is empty 
-	 * @param nums, array of integers
+	/**
+	 * returns the number of clumps in input array, throws assertion error if array
+	 * is empty
+	 * @param arrayOfIntegers, array of integers
 	 * @return noOfClumps, throws assertion error if array is empty
 	 */
-	public int countClumps(int[] nums) {
-		final int len = nums.length;
-		int count = 0;
-		if(nums.length==0)
+	public int countClumps(int[] arrayOfIntegers) {
+		
+		final int LENGTH_OF_ARRAY = arrayOfIntegers.length;
+		int noOfClumps = 0;
+		
+		if (LENGTH_OF_ARRAY == 0) 
 		{
 			throw new AssertionError("Assertion Error Occured: an empty array passed");
 		}
-		for (int i = 0; i <= len - 2; i++) {
-			if (nums[i] == nums[i + 1]) {
-				count++;
-				i++;
+		
+		// find no. of clumps
+		for (int index = 0; index <= LENGTH_OF_ARRAY - 2; index++) 
+		{
+			if (arrayOfIntegers[index] == arrayOfIntegers[index + 1]) 
+			{
+				noOfClumps++;
+				index++;
 			}
-			while (i<len-2 && nums[i] ==nums[i+1]) {
-				i++;
+			while (index < LENGTH_OF_ARRAY - 2 && arrayOfIntegers[index] == arrayOfIntegers[index + 1]) 
+			{
+				index++;
 			}
 
-		}    
-		return count;
+		}
+		return noOfClumps;
 	}
 
-	/**returns the split index so that sum of the number on one side is 
-	 * equal to the sum of the numbers on the other side
-	 * @param nums, array of Integers
-	 * @return splitIndex, if found any split position then return split position o/w
-	 *         returns -1
+	/**
+	 * returns the split index so that sum of the number on one side is equal to the
+	 * sum of the numbers on the other side
+	 * @param arrayOfIntegers, array of Integers
+	 * @return splitIndex, if found any split position then return split position
+	 * o/w returns -1
 	 */
-	public int findSplitArrayIndex(int nums[]) {
-		int leftOfSplitSum = 0;
-		int rightOfSplitSum = 0;
-		int arrayLength = nums.length;
+	public int findSplitArrayIndex(int arrayOfIntegers[]) {
 		
-		if(nums.length==0)
+		int sumOfLeftOfSplit = 0;
+		int sumOfRightFromSplit = 0;
+		int LENGTH_OF_ARRAY = arrayOfIntegers.length;
+
+		if (arrayOfIntegers.length == 0) 
 		{
 			throw new AssertionError("Assertion Error Occured: an empty array passed");
 		}
-		for (int index = 1; index < arrayLength; index++) {
-			
+		for (int index = 1; index < LENGTH_OF_ARRAY; index++) 
+		{
+
 			// sum of left side
 			for (int currentIndexOfLeftSplit = 0; currentIndexOfLeftSplit < index; currentIndexOfLeftSplit++)
-				leftOfSplitSum += nums[currentIndexOfLeftSplit];
+				sumOfLeftOfSplit += arrayOfIntegers[currentIndexOfLeftSplit];
 
-			// sum of elements on rigth sid                      e
-			for (int currentIndexOfRightSplit = index; currentIndexOfRightSplit < arrayLength; currentIndexOfRightSplit++)
-				rightOfSplitSum += nums[currentIndexOfRightSplit];
+			// sum of elements on rigth side
+			for (int currentIndexOfRightSplit = index; currentIndexOfRightSplit < LENGTH_OF_ARRAY; currentIndexOfRightSplit++)
+				sumOfRightFromSplit += arrayOfIntegers[currentIndexOfRightSplit];
 
 			// check if left side sum is equal to right side
-			if (leftOfSplitSum == rightOfSplitSum)
-				return index ;
-			else {
-				rightOfSplitSum = 0;
-				leftOfSplitSum = 0;
+			if (sumOfLeftOfSplit == sumOfRightFromSplit)
+				return index;
+			else 
+			{
+				sumOfRightFromSplit = 0;
+				sumOfLeftOfSplit = 0;
 			}
 		}
-		// if it is not possible to split array into
-		// two parts
+		
+		/* if it is not possible to split array into
+		two parts with equal sum on both side then simply return -1 */
 		return -1;
 	}
-
 	
+
 	/**
 	 * returns an array that contains exactly the same numbers as the input array,
-	 * ,but rearranged so that every X is immediately followed by a Y, requires that X is fixed at its position 
-	 * we can move Y from its position.
-	 * @param nums, array of integers
+	 * ,but rearranged so that every X is immediately followed by a Y, requires that
+	 * X is fixed at its position we can move Y from its position.
+	 * @param arrayOfIntegers, array of integers
 	 * @param x, X value
 	 * @param y, Y value
 	 * @return returns rearranged array according to above specification
 	 */
-	public int[] fixXY(int[] nums, int x, int y) {
-		int j = 1;
+	public int[] fixXY(int[] arrayOfIntegers, int x, int y) {
+		int index2 = 1;
 		int noOfX = 0;
 		int noOfY = 0;
-		if(nums.length==0)
+		final int LENGTH_OF_ARRAY = arrayOfIntegers.length;
+		if (LENGTH_OF_ARRAY == 0) 
 		{
 			throw new AssertionError("Assertion Error Occured: an empty array passed");
 		}
-		
+
 		// count noOfX and noOfY
-		for (int index = 0; index < nums.length;index++)
+		for (int index = 0; index < LENGTH_OF_ARRAY; index++) 
 		{
-			if(nums[index]==x)
+			if (arrayOfIntegers[index] == x) 
 			{
 				noOfX++;
 				
 				// if adjacent X values are found throw error
-				if(nums[index]==nums[index+1])
+				if (arrayOfIntegers[index] == arrayOfIntegers[index + 1])
 				{
 					throw new AssertionError("Assertion Error occured: adjacent X values are there");
 				}
 			}
-			if(nums[index]==y)
+			if (arrayOfIntegers[index] == y) 
 			{
 				noOfY++;
 			}
-			
+
 		}
-		
+
 		// if unequal number of X and Y occurs throw error
-		if(noOfX!=noOfY){
+		if (noOfX != noOfY) 
+		{
 			throw new AssertionError("Assertion Error Occured: unequal number of X and Y ");
 		}
-		
+
 		// if X occurs at last index of array throw Assertion error
-		if(nums[nums.length-1]==x)
+		if (arrayOfIntegers[LENGTH_OF_ARRAY - 1] == x) 
 		{
 			throw new AssertionError("Asertion Error occured: x occurs at the last index of array");
 		}
 
-		for (int i = 0; i < nums.length - 1; i++) {
-			if (nums[i] == x && nums[i + 1] != y) {
-				while (nums[j] != y) {
+		// rearranging array according to above specification
+		
+		index2 = 1;
+		
+		for (int index1 = 0; index1 < LENGTH_OF_ARRAY - 1; index1++) 
+		{
+			if (arrayOfIntegers[index1] == x && arrayOfIntegers[index1 + 1] != y) 
+			{
+				while (arrayOfIntegers[index2] != y) 
+				{
 					// finds the index of y
-					if (j >= nums.length - 1)
-						j = 0;
+					if ( index2 >= LENGTH_OF_ARRAY - 1)
+						index2 = 0;
 					else
-						j++;
+						index2++;
 
 				}
-				nums[j] = nums[i + 1];
-				nums[i + 1] = y;
+				arrayOfIntegers[index2] = arrayOfIntegers[index1 + 1];
+				arrayOfIntegers[index1 + 1] = y;
 			}
 		}
-		return nums;
+		return arrayOfIntegers;
 	}
 }
