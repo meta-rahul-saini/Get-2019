c46585de63da50ff3dd0304ffe6d6d0e8051ff55
meta-rahul-saini <52689695+meta-rahul-saini@users.noreply.github.com>
2019-07-22 04:21:07 +0530

    Update NegativeTest.java

--- ASSIGNMENT4/src/NegativeTest.java
+++ ASSIGNMENT4/src/NegativeTest.java
@@ -8,21 +8,45 @@ public class NegativeTest {
 		 arrOperations = new ArrOperations();
 	 }
 	 
-	 @Test(expected = AssertionError.class)
+	 
+	 
+	 /**
+	 * if array is empty:  throw assertion error.
+	 * else :  returns max mirror in array
+	 * 
+	 */
+	@Test(expected = AssertionError.class)
 	 public void maxMirrorAE() {
 		 arrOperations.findMaxMirror(new int[]{});
 	 }
 	   
+
+	 /**
+	 * if array is empty:  throw assertion error.
+	 * else :   returns no of clumps in array
+	 */
 	 @Test(expected = AssertionError.class)
 	 	public void countClumpsAE() { 
 		arrOperations.countClumps(new int[]{});
 	 }
 	   
+	 /**
+	 * if array is empty:      throw assertion error.
+	 * if unequal no. of x and Y:   throw assertion error.
+	 * if two adjacent X values are there: 	throw assertion error
+	 * if X occurs at last index of array: 	throw assertion error
+	 * else    :   returns no of clumps in array
+	 */
 	 @Test(expected = AssertionError.class)
 	 	public void fixXYAE() { 
 		arrOperations.fixXY(new int[]{}, 0 , 0);
 	 }
 	   
+
+	 /**
+	 * if array is empty:   throw assertion error.
+	 * else :   returns split index in array
+	 */
 	 @Test(expected = AssertionError.class)
 	 	public void splitArrayAE() { 
 		 arrOperations.findSplitArrayIndex(new int[]{});
