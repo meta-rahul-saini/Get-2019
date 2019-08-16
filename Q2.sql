2f57f0b21f3f3ebb7d18933441cdfb925c013f94
meta-rahul-saini <52689695+meta-rahul-saini@users.noreply.github.com>
2019-08-16 21:16:16 +0530

    Update Q2.sql

--- Q2.sql
+++ Q2.sql
@@ -1,1 +1,47 @@
+Assignment 2:
+Write SQL scripts for the following:
 
+2.1 Display the list of products (Id, Title, Count of Categories) which fall in more than one Categories.
+
+select 
+    product_id, product_title, count(*)
+from
+    product
+group by product_title;
+
+
+
+2.2 Display Count of products as per below price range:
+
+SELECT 
+    count(*)
+FROM
+    product
+WHERE
+    unit_price BETWEEN 0 and 100;
+    
+SELECT 
+    count(*)
+FROM
+    product
+WHERE
+    unit_price BETWEEN 101 and 500;
+
+SELECT 
+    count(*)
+FROM
+    product
+WHERE
+    unit_price > 500;
+	
+  
+  
+2.3 Display the Categories along with number of products under each category.
+
+select 
+    category_name, count(product.category_id) as no_of_product
+from
+    product
+        inner join
+    category ON product.category_id = category.category_id
+group by product.category_id;
