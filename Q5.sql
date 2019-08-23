a085242906c470ef9f28a588fc34a496d78fb1d5
meta-rahul-saini <52689695+meta-rahul-saini@users.noreply.github.com>
2019-08-23 14:37:57 +0530

    Update Q5.sql

--- Q5.sql
+++ Q5.sql
@@ -3,14 +3,25 @@ Assignment 5:
 5.1 Create a view displaying the order information (Id, Title, Price, Shopper’s name, Email, Orderdate, Status) with latest ordered items should be displayed first for last 60 days.
 
 drop view if exists order_view;
-create view order_view as select o.order_id, o.order_amount, u.user_name, u.email_id, o.order_date, o.order_status, o.product_id from storefrontdb.order as o inner join user as u on o.user_id = u.user_id order by o.order_date >= DATE_SUB('2019-08-08', INTERVAL 2 MONTH) desc;
 
-select * from order_view ;
+create view order_info as select o.order_id, product_title, unit_price, user_name, email_id, order_date, order_status from storefrontdb.order as o inner join products_in_order as p on o.order_id = p.order_id inner join product on p.product_id = product.product_id inner join user on o.user_id = user.user_id order by order_date desc;
 
 5.2 Use the above view to display the Products(Items) which are in ‘shipped’ state.
 
-select product_title, order_id from order_view inner join product using (product_id) where order_status='shipped';
+select 
+    *
+from
+    order_info
+where
+    order_status = 'shipped';
 
 5.3 Use the above view to display the top 5 most selling products.
 
-select count(product_id), product_title from order_view inner join product using (product_id) order by product_id desc limit 5;
+
+select 
+    product_title, count(product_title) as count
+from
+    order_info
+group by product_title
+order by count desc
+limit 5;
