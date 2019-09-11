25f6b6c76e97615a859c4c471b97dd3a99886e40
meta-rahul-saini <52689695+meta-rahul-saini@users.noreply.github.com>
2019-09-11 18:36:44 +0530

    Add files via upload

---
+++ java/com/metacube/SpringMVCStudentDetails/SpringMvcStudentDetailsApplication.java
@@ -0,0 +1,13 @@
+package com.metacube.SpringMVCStudentDetails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcStudentDetailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcStudentDetailsApplication.class, args);
+	}
+
+}
