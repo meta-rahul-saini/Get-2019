a43ecdc761348ebbc6b080d061682ed94b074b97
meta-rahul-saini <52689695+meta-rahul-saini@users.noreply.github.com>
2019-09-13 11:46:12 +0530

    Add files via upload

---
+++ mvc1Final/Mvc1FinalApplication.java
@@ -0,0 +1,13 @@
+package com.metacube.mvc1Final;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mvc1FinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mvc1FinalApplication.class, args);
+	}
+
+}
